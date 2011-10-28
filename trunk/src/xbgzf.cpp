a0da1c915fdc6eb10f0c5bae09a7ee297c79b96e
plindenbaum.u915 <plindenbaum.u915@b48bb709-da2a-0096-c5f6-d1abd257d063>
2011-10-28 06:55:01 +0000

    cont

---
+++ trunk/src/xbgzf.cpp
@@ -0,0 +1,71 @@
+/*
+ * xbgzf.cpp
+ *
+ *  Created on: Oct 28, 2011
+ *      Author: lindenb
+ */
+#include <sstream>
+#include <cstring>
+#include <cerrno>
+#include <stdexcept>
+#include <bgzf.h>
+#include "xbgzf.h"
+using namespace std;
+
+#define CASTPTR ((BGZF *)(this->_ptr))
+
+
+BgzFile::BgzFile(const char* filename,const char* mode):_ptr(NULL)
+    {
+    _ptr=::bgzf_open(filename,mode);
+    if(_ptr==NULL)
+	{
+	ostringstream os;
+	os << "Cannot open "<< filename << " " << strerror(errno);
+	throw runtime_error(os.str());
+	}
+    }
+BgzFile::BgzFile(int fd,const char* mode):_ptr(NULL)
+    {
+    _ptr=::bgzf_fdopen(fd,mode);
+    if(_ptr==NULL)
+   	{
+   	ostringstream os;
+   	os << "Cannot open fd " << strerror(errno);
+   	throw runtime_error(os.str());
+   	}
+    }
+BgzFile::~BgzFile()
+    {
+    close();
+    }
+void BgzFile::close()
+    {
+    if(_ptr!=NULL) ::bgzf_close(CASTPTR);
+    _ptr=NULL;
+    }
+int BgzFile::read(void* data,int length)
+    {
+    return _ptr==NULL? -1:bgzf_read(CASTPTR,data,length);
+    }
+
+int BgzFile::write(void* data,int length)
+    {
+    return _ptr==NULL? -1:bgzf_write(CASTPTR,data,length);
+    }
+int64_t BgzFile::tell()
+    {
+    return _ptr==NULL? -1:bgzf_tell(CASTPTR);
+    }
+int64_t BgzFile::seek(int64_t pos, int where)
+    {
+    return _ptr==NULL? -1:bgzf_seek(CASTPTR,pos,where);
+    }
+void BgzFile::set_cache_size(int cache_size)
+    {
+    if( _ptr!=NULL) ::bgzf_set_cache_size(CASTPTR,cache_size);
+    }
+int BgzFile::getc()
+    {
+    return _ptr==NULL? -1:bgzf_getc(CASTPTR);
+    }
---
+++ trunk/src/xbgzf.h
@@ -0,0 +1,29 @@
+/*
+ * xbgzf.h
+ *
+ *  Created on: Oct 28, 2011
+ *      Author: lindenb
+ */
+
+#ifndef XBGZF_H_
+#define XBGZF_H_
+
+#include <stdint.h>
+class BgzFile
+    {
+    private:
+	void* _ptr;
+    public:
+	BgzFile(int fd,const char* m);
+	BgzFile(const char* filename,const char* m);
+	virtual ~BgzFile();
+	virtual void close();
+	virtual int read(void* data,int length);
+	virtual int write(void* data,int length);
+	virtual int64_t tell();
+	virtual int64_t seek(int64_t pos, int where);
+	virtual void set_cache_size(int cache_size);
+	virtual int getc();
+    };
+
+#endif /* XBGZF_H_ */
