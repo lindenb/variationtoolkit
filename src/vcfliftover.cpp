b3dc3c971126bc3ba38b89d8dc81bbab178ffc2b
plindenbaum.u915 <plindenbaum.u915@b48bb709-da2a-0096-c5f6-d1abd257d063>
2011-11-09 11:27:25 +0000

    cont

--- src/vcfliftover.cpp
+++ src/vcfliftover.cpp
@@ -27,12 +27,13 @@ class VcfLiftOver:public AbstractApplication
 	LiftOver* liftOver;
 	int chromCol;
 	int posCol;
-
+	bool oneBased;
 
 	VcfLiftOver():liftOver(NULL)
 	    {
 	    chromCol=0;
 	    posCol=1;
+	    oneBased=true;
 	    }
 	~VcfLiftOver()
 	    {
@@ -44,65 +45,66 @@ class VcfLiftOver:public AbstractApplication
 	    {
 	    vector<string> tokens;
 	    string line;
+	    int shift=(oneBased?1:0);
 	    while(getline(in,line,'\n'))
-		{
-		if(line.empty()) continue;
-		tokenizer.split(line,tokens);
-		if(line[0]=='#')
 			{
-			if(line.size()>1 && line[1]=='#')
+			if(line.empty()) continue;
+			tokenizer.split(line,tokens);
+			if(line[0]=='#')
 				{
-				cout << line << endl;
+				if(line.size()>1 && line[1]=='#')
+					{
+					cout << line << endl;
+					continue;
+					}
+				cout << line ;
+				cout << tokenizer.delim << "lift.chrom";
+				cout << tokenizer.delim << "lift.start";
+				cout << tokenizer.delim << "lift.end";
+				cout << tokenizer.delim << "lift.error";
+				cout << endl;
 				continue;
 				}
-			cout << line ;
-			cout << tokenizer.delim << "lift.chrom";
-			cout << tokenizer.delim << "lift.start";
-			cout << tokenizer.delim << "lift.end";
-			cout << tokenizer.delim << "lift.error";
-			cout << endl;
-			continue;
+			if(chromCol>=(int)tokens.size())
+				{
+				cerr << "CHROM out of range in " << line << endl;
+				continue;
+				}
+			if(posCol>=(int)tokens.size())
+				{
+				cerr << "POS out of range in " << line << endl;
+				continue;
+				}
+			char* p2;
+			int chromStart=(int)strtol(tokens[posCol].c_str(),&p2,10);
+			if(*p2!=0)
+				{
+				cerr << "BAD POS  in " << line << endl;
+				continue;
+				}
+
+			ChromStartEnd segment(tokens[chromCol],chromStart-shift,chromStart);
+			std::auto_ptr<ChromStartEnd> seg = this->liftOver->convert(&segment);
+			if(seg.get()==NULL)
+				{
+				const char* err=  this->liftOver->lastError();
+				cout << line ;
+				cout << tokenizer.delim << ".";
+				cout << tokenizer.delim << ".";
+				cout << tokenizer.delim << ".";
+				cout << tokenizer.delim << (err==NULL?"#ERR":err);
+				cout << endl;
+				}
+			else
+				{
+				cout << line ;
+				cout << tokenizer.delim << seg->chrom;
+				cout << tokenizer.delim << (seg->start+shift);
+				cout << tokenizer.delim << (seg->end+shift);
+				cout << tokenizer.delim << ".";
+				cout << endl;
+				}
 			}
-		if(chromCol>=(int)tokens.size())
-			{
-			cerr << "CHROM out of range in " << line << endl;
-			continue;
-			}
-		if(posCol>=(int)tokens.size())
-			{
-			cerr << "POS out of range in " << line << endl;
-			continue;
-			}
-		char* p2;
-		int chromStart0=(int)strtol(tokens[posCol].c_str(),&p2,10);
-		if(*p2!=0)
-		    {
-		    cerr << "BAD POS  in " << line << endl;
-		    continue;
-		    }
-		chromStart0--;
-		ChromStartEnd segment(tokens[chromCol],chromStart0,chromStart0+1);
-		std::auto_ptr<ChromStartEnd> seg = this->liftOver->convert(&segment);
-		if(seg.get()==NULL)
-		    {
-		    const char* err=  this->liftOver->lastError();
-		    cout << line ;
-		    cout << tokenizer.delim << ".";
-		    cout << tokenizer.delim << ".";
-		    cout << tokenizer.delim << ".";
-		    cout << tokenizer.delim << (err==NULL?"#ERR":err);
-		    cout << endl;
-		    }
-		else
-		    {
-		    cout << line ;
-		    cout << tokenizer.delim << seg->chrom;
-		    cout << tokenizer.delim << seg->start;
-		    cout << tokenizer.delim << seg->end;
-		    cout << tokenizer.delim << ".";
-		    cout << endl;
-		    }
-		}
 	    }
 	void usage(std::ostream& out,int argc,char** argv)
 		{
@@ -114,87 +116,127 @@ class VcfLiftOver:public AbstractApplication
 		out << "  -d <char> column delimiter. default: TAB\n";
 		out << "  -c <int> chromosome column ("<< (chromCol+1) << ").\n";
 		out << "  -p <int> pos column ("<< (posCol+1) << ").\n";
-		out << "  -f <filename> liftOver file (required).\n";
+		out << "  -1 data are NOT +1 based.\n";
+		out << "Lift over parameters:\n";
+		out << "  -f (path) liftOver map file (required).\n";
+		out << "  -b (double) liftOver minblocks.\n";
+		out << "  -m (double) liftOver minMatch.\n";
 		out << endl;
 		out << endl;
 		}
 
 	int main(int argc, char *argv[])
 	    {
+		char* minBlocks=NULL;
+		char* minMatch=NULL;
 	    char* mapFile=NULL;
 	    int optind=1;
 	    /* loop over the arguments */
 	    while(optind<argc)
-		{
-		if(strcmp(argv[optind],"-h")==0)
-		    {
-		    usage(cerr,argc,argv);
-		    return EXIT_SUCCESS;
-		    }
-		else if(strcmp(argv[optind],"-f")==0 && optind+1< argc)
-		    {
-		    mapFile=argv[++optind];
-		    }
-		else if(strcmp(argv[optind],"-d")==0 && optind+1< argc)
-		    {
-		    if(strlen(argv[optind+1])!=1)
 			{
-			fprintf(stderr,"Expected only one char for the delimiter.\n");
-			return EXIT_FAILURE;
+			if(strcmp(argv[optind],"-h")==0)
+				{
+				usage(cerr,argc,argv);
+				return EXIT_SUCCESS;
+				}
+			else if(strcmp(argv[optind],"-1")==0)
+				{
+				oneBased=false;
+				}
+			else if(strcmp(argv[optind],"-f")==0 && optind+1< argc)
+				{
+				mapFile=argv[++optind];
+				}
+			else if(strcmp(argv[optind],"-d")==0 && optind+1< argc)
+				{
+				if(strlen(argv[optind+1])!=1)
+					{
+					fprintf(stderr,"Expected only one char for the delimiter.\n");
+					return EXIT_FAILURE;
+					}
+				tokenizer.delim=argv[++optind][0];
+				}
+			else if(strcmp(argv[optind],"-c")==0 && optind+1< argc)
+				{
+				char* p2=NULL;
+				chromCol=strtol(argv[++optind],&p2,10);
+				if(chromCol<1 ||  *p2!=0)
+					{
+					cerr << "Bad column for CHROM: "<< argv[optind] << endl;
+					usage(cerr,argc,argv);
+					return EXIT_FAILURE;
+					}
+				chromCol--;
+				}
+			else if(strcmp(argv[optind],"-p")==0 && optind+1< argc)
+				{
+				char* p2=NULL;
+				posCol=strtol(argv[++optind],&p2,10);
+				if(posCol<1 ||  *p2!=0)
+					{
+					cerr << "Bad column for POS: "<< argv[optind] << endl;
+					usage(cerr,argc,argv);
+					return EXIT_FAILURE;
+					}
+				posCol--;
+				}
+			else if(strcmp(argv[optind],"-b")==0 && optind+1< argc)
+				{
+				minBlocks=argv[++optind];
+				}
+			else if(strcmp(argv[optind],"-m")==0 && optind+1< argc)
+				{
+				minMatch=argv[++optind];
+				}
+			else if(strcmp(argv[optind],"--")==0)
+				{
+				optind++;
+				break;
+				}
+			else if(argv[optind][0]=='-')
+				{
+				cerr << "Unknown option: "<< argv[optind]<< "\n";
+				usage(cerr,argc,argv);
+				return EXIT_FAILURE;
+				}
+			else
+				{
+				break;
+				}
+			++optind;
 			}
-		    tokenizer.delim=argv[++optind][0];
-		    }
-		else if(strcmp(argv[optind],"-c")==0 && optind+1< argc)
-		    {
-		    char* p2=NULL;
-		    chromCol=strtol(argv[++optind],&p2,10);
-		    if(chromCol<1 ||  *p2!=0)
-			{
-			cerr << "Bad column for CHROM: "<< argv[optind] << endl;
-			usage(cerr,argc,argv);
-			return EXIT_FAILURE;
-			}
-		    chromCol--;
-		    }
-		else if(strcmp(argv[optind],"-p")==0 && optind+1< argc)
-		    {
-		    char* p2=NULL;
-		    posCol=strtol(argv[++optind],&p2,10);
-		    if(posCol<1 ||  *p2!=0)
-			{
-			cerr << "Bad column for POS: "<< argv[optind] << endl;
-			usage(cerr,argc,argv);
-			return EXIT_FAILURE;
-			}
-		    posCol--;
-		    }
-		else if(strcmp(argv[optind],"--")==0)
-		    {
-		    optind++;
-		    break;
-		    }
-		else if(argv[optind][0]=='-')
-		    {
-		    cerr << "Unknown option: "<< argv[optind]<< "\n";
-		    usage(cerr,argc,argv);
-		    return EXIT_FAILURE;
-		    }
-		else
-		    {
-		    break;
-		    }
-		++optind;
-		}
 	    if(mapFile==NULL)
-		{
-		cerr << "Error: undefined map file file.\n" << endl;
-		usage(cerr,argc,argv);
-		return EXIT_FAILURE;
-		}
+			{
+			cerr << "Error: undefined map file file.\n" << endl;
+			usage(cerr,argc,argv);
+			return EXIT_FAILURE;
+			}
 
 
 
 	    liftOver=new LiftOver(mapFile);
+
+	    if(minMatch!=NULL)
+	    	{
+	    	char* p2;
+	    	liftOver->minMatch(strtod(minMatch,&p2));
+	    	if(*p2!=0)
+	    		{
+	    		cerr << "Error with param 'minMatch' "<< minMatch << endl;
+	    		return EXIT_FAILURE;
+	    		}
+	    	}
+
+	    if(minBlocks!=NULL)
+			{
+			char* p2;
+			liftOver->minBlocks(strtod(minBlocks,&p2));
+			if(*p2!=0)
+				{
+				cerr << "Error with param 'minBlocks' "<< minBlocks << endl;
+				return EXIT_FAILURE;
+				}
+			}
 
 	    if(optind==argc)
 			{
