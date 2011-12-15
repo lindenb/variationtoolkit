0028bce920c45bd2a18977d525396f5ba5b94dd4
plindenbaum.u915 <plindenbaum.u915@b48bb709-da2a-0096-c5f6-d1abd257d063>
2011-12-15 11:25:10 +0000

    cont

--- src/redonstructvar.cpp
+++ src/redonstructvar.cpp
@@ -9,6 +9,9 @@
 #include <cstring>
 #include <cstdlib>
 #include <memory>
+#include <algorithm>
+#include <set>
+#include <map>
 #include "bam.h"
 #include "throw.h"
 #include "auto_vector.h"
@@ -16,6 +19,8 @@
 #include "knowngene.h"
 #include "application.h"
 #include "zstreambuf.h"
+#include "loess.h"
+#include "where.h"
 using namespace std;
 
 extern "C" {
@@ -29,12 +34,14 @@ class RedonStructVar:public AbstractApplication
 
 	class BamFile;
 
-	struct Param
+	struct DepthShuttle
 	    {
 	    RedonStructVar* owner;
-	    Exon* exon;
 	    BamFile* bamFile;
-	    vector<int>* coverage;
+	    const char* chrom;
+	    int32_t start;
+	    int32_t end;
+	    double coverage;
 	    };
 
 	struct GCPercent
@@ -43,10 +50,14 @@ class RedonStructVar:public AbstractApplication
 	    double total;
 	    };
 
-	struct Point
+	struct CoverageAndGc
 	    {
 	    double gc;
-	    int32_t coverage;
+	    double coverage;
+	    bool operator< (const CoverageAndGc& cp) const
+		{
+		return gc < cp.gc;
+		}
 	    };
 
 
@@ -58,11 +69,14 @@ class RedonStructVar:public AbstractApplication
 		return 0;
 	    }
 
-	static int  scan_all_genome_func(uint32_t tid, uint32_t pos, int depth, const bam_pileup1_t *pl, void *data)
+
+
+	/**
+	static int  _deprecated_scan_all_genome_func(uint32_t tid, uint32_t pos, int depth, const bam_pileup1_t *pl, void *data)
 	    {
-	    Param* param=( Param*)data;
-	    if(pos<param->exon->start) return 0;
-	    if(pos>=param->exon->end) return 0;
+	    DepthShuttle* param=( DepthShuttle*)data;
+	    if(pos<param->position) return 0;
+	    if(pos>=pos) return 0;
 	    cerr << "seq:"<< param->bamFile->header->target_name[tid] << " pos:" << pos << " dep:"<< depth << endl;
 	    for( int i=0;i<depth ;++i)
 		{
@@ -153,18 +167,18 @@ class RedonStructVar:public AbstractApplication
 		    {
 		    cerr << (char)(qualities[n]+33);
 		    }
-		 /*
+
 		for(int n=0;n< core.l_qseq;++n)
 		    {
 		    cerr << bam_nt16_rev_table[bam1_seqi(seq,n)];
 		    }
-		cerr << " ";*/
-		/*
+		cerr << " ";
+
 		cerr << " qpos:" <<  p->qpos;
 		cerr << " indel:" <<  p->indel;
 		cerr << " is_del :" <<  p->is_del;
 		cerr << " strand:" <<  bam1_strand(aln);
-		*/
+
 		cerr << " cigar:" << core.n_cigar;
 
 
@@ -172,8 +186,73 @@ class RedonStructVar:public AbstractApplication
 		}
 	    cerr << endl;
 	    return 0;
-	    }
+	    }*/
 
+
+	static int  scan_all_genome_func(uint32_t tid, uint32_t pos, int depth, const bam_pileup1_t *pl, void *data)
+	    {
+	    DepthShuttle* param=( DepthShuttle*)data;
+	    //WHERE("ici!"<< pos << " " << param->chrom << ":" << param->position);
+	    if((int32_t)pos<param->start) return 0;
+	    if((int32_t)pos>=param->end) return 0;
+	    //WHERE("ici!"<< pos << " vs " << param->chrom << ":" << param->position << " d="<< depth);
+	    for( int i=0;i<depth ;++i)
+		{
+		const bam_pileup1_t *p=&pl[i];
+		bam1_t* aln=p->b;
+		const bam1_core_t& core=aln->core;
+		uint32_t *cigar = bam1_cigar(aln);
+		//const uint8_t* seq=bam1_seq(aln);
+		uint8_t *qualities = bam1_qual(aln);
+		int read_seq_index=0;
+		uint32_t genomic_pos=core.pos;
+
+		for (uint32_t cigaridx = 0; cigaridx < core.n_cigar; ++cigaridx)
+		    {
+		    int op = cigar[cigaridx] & BAM_CIGAR_MASK;
+		    int n_op= cigar[cigaridx] >> BAM_CIGAR_SHIFT;
+		    for(int k=0;k< n_op;++k)
+			{
+			switch(op)
+			    {
+			    case BAM_CMATCH:
+				{
+				if(genomic_pos==pos)
+				    {
+				    uint8_t qual=qualities[read_seq_index];
+				    if(param->owner->min_qual<=(double)qual)
+					{
+					param->coverage++;
+					}
+				    }
+				++read_seq_index;
+				++genomic_pos;
+				break;
+				}
+			    //deletion from the reference
+			    case BAM_CDEL:
+				{
+				++genomic_pos;
+				break;
+				}
+			    //insertion to the reference
+			    case BAM_CSOFT_CLIP:
+			    case BAM_CINS:
+				{
+				++read_seq_index;
+				break;
+				}
+			    default:
+				{
+				cerr << "####cigar::op not handled:" << op << endl ;
+				break;
+				}
+			    }
+			}
+		    }
+		}
+	    return 0;
+	    }
 
 
 	class BamFile
@@ -232,74 +311,236 @@ class RedonStructVar:public AbstractApplication
 		    return ::bam_get_tid(header, seq_name);
 		    }
 
-		void pileup(Param* param)
+		bool pileup(DepthShuttle* param)
 		    {
-		    const char* chrom=param->exon->gene->chrom.c_str();
-		    int32_t tid=getTidByName(chrom);
-		    if(tid<0) THROW("Cannot find chromosome \""<< chrom << "\" in "<< filename);
+		    int32_t tid=getTidByName(param->chrom);
+
+		    if(tid<0)
+			{
+			cerr << "Cannot find chromosome \""<< param->chrom << "\". Check you're using the ucsc nomenclature.\n";
+			return false;
+			}
+		    if(param->end >= (int32_t)header->target_len[tid])
+			{
+			return false;
+			}
 
 		    ::bam_plbuf_t *buf= ::bam_plbuf_init( scan_all_genome_func,param);
-		    ::bam_fetch(in, idx, tid, param->exon->start, param->exon->end, buf, fetch_func);
+		    if(buf==NULL) THROW("boum");
+		    ::bam_fetch(in, idx, tid, param->start, param->end, buf, fetch_func);
 		    ::bam_plbuf_push(0, buf); // finalize pileup
 		    ::bam_plbuf_destroy(buf);
+		    return true;
 		    }
 
 	    };
-
-	std::string genomePath;
+	Loess loessAlgo;
 	auto_vector<BamFile> bamFiles;
 	std::auto_ptr<IndexedFasta> faidx;
+	auto_vector<KnownGene> knownGenes;
 	double min_qual;
+
 	RedonStructVar():min_qual(0.0)
 	    {
 	    }
 
 
-	GCPercent getGC(const Exon* exon)
+	bool coverageAt(const char* chrom,int32_t exon_start,int32_t exon_end,double* coverage)
+	    {
+	    DepthShuttle param;
+	    memset((void*)&param,0,sizeof(DepthShuttle));
+	    param.chrom=chrom;
+	    param.owner=this;
+	    param.start=exon_start;
+	    param.end=exon_end;
+	    for(size_t b=0;b < bamFiles.size();++b)
+		{
+		BamFile* bf=bamFiles.at(b);
+		param.bamFile=bf;
+		if(!bf->pileup(&param)) return false;
+		}
+	    param.coverage/=(double)(exon_end-exon_start);
+	    param.coverage/=(double)bamFiles.size();
+	    *coverage=param.coverage;
+	    return true;
+	    }
+
+	bool gcPercentAt(const char* chrom,int32_t exon_start,int32_t exon_end,double* gcpercent)
 	    {
 	    GCPercent g;
 	    std::memset((void*)&g,0,sizeof(GCPercent));
-	    std::auto_ptr<string> seq=faidx->fetch(exon->gene->chrom.c_str(),exon->start,exon->end);
-	    if(seq.get()!=0)
+	    std::auto_ptr<string> seq=faidx->fetch(chrom,exon_start,exon_end);
+	    if(seq.get()==0) return false;
+	    //if(seq->size()!=1) cerr << "???GC%:"<< chrom<<":" << pos << endl;
+	    for(size_t i=0;i<seq->size();++i)
 		{
-		for(size_t i=0;i<seq->size();++i)
+		switch(toupper(seq->at(i)))
 		    {
-		    switch(toupper(seq->at(i)))
+		    case 'A':case 'T': case 'W': g.total++;break;
+		    case 'G':case 'C': case 'S': g.gc++;g.total++;break;
+		    default:break;
+		    }
+		}
+
+	    if(g.total==0) return false;
+	    *gcpercent=g.gc/g.total;
+	    return true;
+	    }
+
+	void run()
+	    {
+	    const int32_t window_size=100;
+	    vector<CoverageAndGc> coverageandgc;
+	    set<string> chromosomes;
+	    //get distinct chromosome
+	    for(size_t i=0;i< knownGenes.size();++i)
+		{
+		chromosomes.insert(knownGenes.at(i)->chrom);
+		}
+	    for(set<string>::iterator r=chromosomes.begin(); r!=chromosomes.end();++r)
+		{
+		WHERE("chromosome"<< (*r));
+		vector<bool> exon_bases;
+		exon_bases.reserve(50000000);
+		for(size_t i=0;i< knownGenes.size();++i)
+		    {
+		    KnownGene* gene=knownGenes.at(i);
+		    if((*r).compare(gene->chrom)!=0) continue;
+		    if((int32_t)exon_bases.size()< gene->txEnd)
 			{
-			case 'A':case 'T': case 'W': g.total++;break;
-			case 'G':case 'C': case 'S': g.gc++;g.total++;break;
-			default:break;
+			exon_bases.resize(gene->txEnd+window_size+1,false);
+			}
+		    for(int32_t e=0;e < gene->countExons();++e)
+			{
+			const Exon* exon= gene->exon(e);
+			for(int32_t pos=max(0,exon->start-window_size);pos<exon->end+window_size;++pos)
+			    {
+			    exon_bases[pos]=true;
+			    }
+			}
+		    }
+		int32_t exon_beg=0;
+		while(exon_beg<(int32_t)exon_bases.size())
+		    {
+		    if(!exon_bases[exon_beg])
+			{
+			exon_beg++;
+			continue;
+			}
+		    int32_t exon_end=1+exon_beg;
+		    while(exon_end< (int32_t)exon_bases.size() && exon_bases[exon_end])
+			{
+			exon_end++;
+			}
+		    CoverageAndGc cov;
+		    if(  coverageAt(r->c_str(),exon_beg,exon_end,&(cov.coverage)) &&
+			gcPercentAt(r->c_str(),exon_beg,exon_end,&(cov.gc)))
+			{
+			WHERE((*r)<<":"<< exon_beg << "-" << exon_end);
+			coverageandgc.push_back(cov);
+			}
+		    exon_beg=exon_end;
+		    }
+		}
+	    WHERE("sorting");
+	    //sort on gc
+	    sort(coverageandgc.begin(),coverageandgc.end());
+
+	    vector<double> x;
+	    vector<double> y;
+	    x.reserve(coverageandgc.size());
+	    y.reserve(coverageandgc.size());
+	    for(vector<CoverageAndGc>::iterator r=coverageandgc.begin();
+		r!=coverageandgc.end();
+		++r)
+		{
+		x.push_back(r->gc);
+		y.push_back(r->coverage);
+		}
+	    /*
+	     * echo -e "set yrange [0.0:150.0]\nset terminal postscript\nset output 'jeter.ps'\nplot 'jeter.dat' using 1:2 title 'f1',  'jeter.dat' using 1:3 title 'f2' " | gnuplot  > jeter.ps; ps2pdf jeter.ps
+	    */
+	    auto_ptr<vector<double> > y_prime=loessAlgo.lowess(&(x.front()),&(y.front()),x.size());
+	    WHERE("saving");
+	    FILE* out=fopen("jeter.dat","w");
+	    fprintf(out,"GC\tDEPTH\tLOESS_DEPTH\n");
+	    for(size_t i=0;i< coverageandgc.size();++i)
+		{
+		fprintf(out,"%f\t%f\t%f\n",coverageandgc[i].gc,coverageandgc[i].coverage,y_prime->at(i));
+		}
+	    fflush(out);
+	    fclose(out);
+
+	    fstream os("jeter.exons.tsv",ios::out);
+	    if(!os.is_open()) THROW("boum");
+
+	    os << "chrom" << "\t"
+		<< "gene.name" << "\t"
+		<< "exon.name" << "\t"
+		<< "exon.start" << "\t"
+		<< "exon.end" << "\t"
+		<< "bam.file" << "\t"
+		<< "gc.percent" << "\t"
+		<< "exon.coverage" << "\t"
+		<< "coverage_all.mean" << "\t"
+		<< "loess(bam.coverage)"
+		<< endl
+		;
+
+	    for(set<string>::const_iterator r=chromosomes.begin(); r!=chromosomes.end();++r)
+		{
+		 DepthShuttle param;
+		memset((void*)&param,0,sizeof(DepthShuttle));
+		param.chrom=r->c_str();
+		param.owner=this;
+
+		for(size_t i=0;i< knownGenes.size();++i)
+		    {
+		    KnownGene* gene=knownGenes.at(i);
+		    if((*r).compare(gene->chrom)!=0) continue;
+		    if(i>5) break;//TODO
+		    for(size_t bamIdx=0;bamIdx< bamFiles.size();++bamIdx)
+			{
+			param.bamFile=bamFiles.at(bamIdx);
+			for(int32_t e=0;e < gene->countExons();++e)
+			    {
+			    const Exon* exon= gene->exon(e);
+			    WHERE(gene->name<< " " << exon->name() << "/"<< gene->countExons());
+			    double gc_percent=0.0;
+			    if(!gcPercentAt(r->c_str(),exon->start,exon->end,&(gc_percent))) continue;
+
+			    param.coverage=0;
+			    param.start=exon->start;
+			    param.end=exon->end;
+			    if(!param.bamFile->pileup(&param)) continue;
+
+			    param.coverage/=(double)(exon->end-exon->start);
+
+			    vector<double>::iterator rgc=lower_bound(x.begin(),x.end(),gc_percent);
+			    if(rgc==x.end()) continue;
+			    size_t coverage_index=std::distance(x.begin(),rgc);
+
+			    os << gene->chrom << "\t"
+				<< gene->name << "\t"
+				<< exon->name() << "\t"
+				<< exon->start << "\t"
+				<< exon->end << "\t"
+				<< param.bamFile->filename << "\t"
+				<< gc_percent << "\t"
+				<< param.coverage << "\t"
+				<< y[coverage_index] << "\t"
+				<< y_prime->at(coverage_index)
+				<< endl
+				;
+			    }
 			}
 		    }
 		}
-	    return g;
+	    os.flush();
+	    os.close();
 	    }
 
-
-	void run(const KnownGene* gene)
-	    {
-	    if(gene->countExons()==0) return;
-	    vector<Point> points;
-	    Param param;
-	    param.owner=this;
-	    //loop over exons
-	    for(int32_t i=0;i< gene->countExons();++i)
-		{
-		param.exon=(Exon*)gene->exon(i);
-		param.coverage=new vector<int>(param.exon->end - param.exon->start,0);
-
-		for(size_t b=0;b < bamFiles.size();++b)
-		    {
-		    BamFile* bf=bamFiles.at(b);
-		    param.bamFile=bf;
-		    bf->pileup(&param);
-		    }
-		delete param.coverage;
-		}
-	    sort(points.begin(),points.end());
-	    }
-
-	void run(std::istream& in)
+	void readKnownGenes(std::istream& in)
 	    {
 	    string line;
 	    while(getline(in,line,'\n'))
@@ -307,10 +548,9 @@ class RedonStructVar:public AbstractApplication
 		if(line.empty() || line[0]=='#') continue;
 		auto_ptr<KnownGene> g=KnownGene::parse(line);
 		if(g.get()==0) continue;
-		run(g.get());
+		knownGenes.push_back(g.release());
 		}
 	    }
-
 
 	virtual void usage(ostream& out,int argc,char** argv)
 	    {
@@ -407,7 +647,7 @@ class RedonStructVar:public AbstractApplication
 		{
 		igzstreambuf buf;
 		istream in(&buf);
-		this->run(in);
+		this->readKnownGenes(in);
 		}
 	    else
 		{
@@ -415,16 +655,16 @@ class RedonStructVar:public AbstractApplication
 		    {
 		    igzstreambuf buf(argv[optind++]);
 		    istream in(&buf);
-		    this->run(in);
+		    this->readKnownGenes(in);
 		    buf.close();
 		    }
 		}
-
+	    run();
 	    for(size_t i=0;i< bamFiles.size();++i)
 		{
 		bamFiles[i]->close();
 		}
-
+	    cerr << "Done." << endl;
 	    return EXIT_SUCCESS;
 	    }
     };
