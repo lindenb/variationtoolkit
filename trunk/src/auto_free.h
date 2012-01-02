ecc394889a9b8d31ea75ca50477bb594f0689f4e
plindenbaum.u915 <plindenbaum.u915@b48bb709-da2a-0096-c5f6-d1abd257d063>
2012-01-02 15:22:50 +0000

    ngs proj

---
+++ trunk/src/auto_free.h
@@ -0,0 +1,44 @@
+#ifndef AUTO_FREE_H
+#define AUTO_FREE_H
+#include <memory>
+#include <cassert>
+
+template<typename T>
+class auto_free
+    {
+    public:
+	 typedef T element_type;
+	 typedef void (*fun) (void*);
+    private:
+	 element_type* _ptr;
+	 fun _fun;
+    public:
+
+	 auto_free(element_type* ptr,fun release):_ptr(ptr),_fun(release)
+	     {
+	     }
+	 ~auto_free()
+	     {
+	     if(_ptr!=0 && _fun!=0) _fun((void*)_ptr);
+	     }
+	 void reset()
+	     {
+	     if(_ptr!=0 && _fun!=0) _fun((void*)_ptr);
+	     _ptr=0;
+	     }
+
+	 element_type* operator->() const throw()
+	       {
+	 	assert(_ptr != 0);
+	 	return _ptr;
+	       }
+	 element_type*
+	      get() const throw() { return _ptr; }
+    private:
+	 auto_free(const auto_free& cp)
+	     {
+	     }
+    };
+
+
+#endif
