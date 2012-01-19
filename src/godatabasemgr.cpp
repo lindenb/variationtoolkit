fe72c26eeaf56e2c0cf0094290123ba64d5d861c
plindenbaum.u915 <plindenbaum.u915@b48bb709-da2a-0096-c5f6-d1abd257d063>
2012-01-19 10:55:19 +0000

    cont

--- src/godatabasemgr.cpp
+++ src/godatabasemgr.cpp
@@ -27,6 +27,20 @@ using namespace std;
 
 class GoDatabaseManager:public AbstractApplication
     {
+    private:
+	set<string> relation_ontology;
+	int32_t goa_keyword_column;
+	int32_t go_acn_column;
+	bool inverse_result;
+	bool is_relation_ontology(const char* s) const
+	    {
+	    if(s==0) return false;
+	    std::string s2(s);
+	    std::string::size_type colon=s2.find(':');
+	    if(colon!=string::npos) s2.erase(0,colon+1);
+	    return relation_ontology.find(s2)!=relation_ontology.end();
+	    }
+
     public:
 	char* dataasename;
 	auto_ptr<Connection> connection;
@@ -46,9 +60,37 @@ class GoDatabaseManager:public AbstractApplication
 
 	       };
 
-	GoDatabaseManager():dataasename(NULL)
+	GoDatabaseManager():goa_keyword_column(-1),
+		go_acn_column(-1),
+		inverse_result(false),
+		dataasename(NULL)
 	    {
-
+		relation_ontology.insert("is_a");
+		relation_ontology.insert("part_of");
+		relation_ontology.insert("has_part");
+		relation_ontology.insert("integral_part_of");
+		relation_ontology.insert("has_integral_part");
+		relation_ontology.insert("proper_part_of");
+		relation_ontology.insert("has_proper_part");
+		relation_ontology.insert("located_in");
+		relation_ontology.insert("location_of");
+		relation_ontology.insert("contained_in");
+		relation_ontology.insert("contains");
+		relation_ontology.insert("adjacent_to");
+		relation_ontology.insert("transformation_of");
+		relation_ontology.insert("transformed_into");
+		relation_ontology.insert("derives_from");
+		relation_ontology.insert("derived_into");
+		relation_ontology.insert("preceded_by");
+		relation_ontology.insert("precedes");
+		relation_ontology.insert("has_participant");
+		relation_ontology.insert("participates_in");
+		relation_ontology.insert("has_agent");
+		relation_ontology.insert("agent_in");
+		relation_ontology.insert("instance_of");
+		relation_ontology.insert("has_improper_part");
+		relation_ontology.insert("improper_part_of");
+		relation_ontology.insert("relationship");
 	    }
 
 
@@ -187,6 +229,55 @@ class GoDatabaseManager:public AbstractApplication
 	    return ret;
 	    }
 
+	void _find_ascendants(
+			 const char* term,
+			 set<string>* terms,
+			 Statement* select_ascendants
+			 )
+	    {
+	    if(terms->find(term)!=terms->end()) return;
+	    terms->insert(term);
+	    select_ascendants->reset();
+	    select_ascendants->bind_string(1,term);
+	    set<string> to_process;
+	    while(select_ascendants->step()!=Statement::DONE)
+		{
+
+		const char* child=select_ascendants->get_string(1);
+		assert(child!=0);
+		if(terms->find(child)!=terms->end()) continue;
+		to_process.insert(child);
+		}
+
+	    for(set<string>::iterator r=to_process.begin();
+		    r!=to_process.end();
+		    ++r)
+		{
+		_find_ascendants(r->c_str(),terms,select_ascendants);
+		}
+	    }
+
+	std::auto_ptr<set<string> > find_ascendants(const char* term,const set<string>& relations)
+	    {
+	    ostringstream os;
+	    os << "select target from TERM2REL where acn=?";
+	    if(!relations.empty())
+		{
+		os << " and (";
+		for(set<string>::const_iterator r=relations.begin();r!=relations.end();++r)
+		    {
+		    if(r!=relations.begin()) os << " or ";
+		    os << " rel=\""<< CEscape(*r) << "\"";
+		    }
+		os << ")";
+		}
+	    string sql(os.str());
+	    std::auto_ptr<set<string> > ret(new set<string>());
+	    auto_ptr<Statement> select_ascendants=this->connection->prepare(sql.c_str());
+	    _find_ascendants(term,ret.get(),select_ascendants.get());
+	    return ret;
+	    }
+
 	static  xmlNodePtr first( xmlNodePtr root,const xmlChar* name)
 	    {
 	    if(root==0) return 0;
@@ -258,13 +349,7 @@ class GoDatabaseManager:public AbstractApplication
 
 		    const char* hash=strchr((const char*)rsrc,'#');
 
-		    if(hash!=0 &&
-			(
-			xmlStrEqual(cur_node->name,BAD_CAST "go:is_a") ||
-			xmlStrEqual(cur_node->name,BAD_CAST "go:part_of") ||
-			xmlStrEqual(cur_node->name,BAD_CAST "go:regulates") ||
-			xmlStrEqual(cur_node->name,BAD_CAST "go:negatively_regulates")
-			))
+		    if(hash!=0 && is_relation_ontology((const char*)cur_node->name))
 			{
 			const char* colon=strchr((const char*)cur_node->name,':');
 			insert_term2rel->reset();
@@ -312,13 +397,105 @@ class GoDatabaseManager:public AbstractApplication
 		insert_goa->bind_string(7,tokens[11].c_str());//DB_Object_Type
 
 		insert_goa->execute();
-
 		}
 
 	    this->connection->commit();
 	    this->connection->compact();
 	    }
 
+	void run_goa(std::istream& in)
+	    {
+	    Tokenizer tab('\t');
+	    vector<string> tokens;
+	    string line;
+	    auto_ptr<Statement> select_goa = this->connection->prepare(
+		   "select DB,DB_Object_ID,DB_Object_Symbol,term,DB_Object_Name,Synonym, DB_Object_Type "
+		   " from GOA where (DB_Object_ID=? or DB_Object_Symbol=? or term=? )"
+		    " "
+		    );
+
+	    for(;;)
+		{
+		if(!getline(in,line,'\n')) break;
+		if(line.empty()) continue;
+		if(line[0]=='#')
+		    {
+		    cout << line << "\t";
+		    cout << "DB\tDB_Object_ID\tDB_Object_Symbol\tterm\tDB_Object_Name,Synonym\tDB_Object_Type";
+		    cout << endl;
+		    continue;
+		    }
+		tab.split(line,tokens);
+		if((int32_t)tokens.size()<=goa_keyword_column)
+		    {
+		    cerr << "column "<< (goa_keyword_column+1)<< " missing in "<< line << endl;
+		    continue;
+		    }
+		bool found=false;
+		if(!tokens[goa_keyword_column].empty())
+		    {
+		    select_goa->reset();
+		    select_goa->bind_string(1,tokens[goa_keyword_column].c_str());
+		    select_goa->bind_string(2,tokens[goa_keyword_column].c_str());
+		    select_goa->bind_string(3,tokens[goa_keyword_column].c_str());
+		    while(select_goa->step()!=Statement::DONE)
+			{
+			found=true;
+			cout << line;
+			for(int c=0;c < select_goa->column_count();++c)
+			    {
+			    cout << "\t";
+			    cout << select_goa->get_string(c+1);
+			    }
+			cout << endl;
+			}
+		    }
+		if(!found)
+		    {
+		    cout << line;
+		    for(int c=0;c < 7;++c)
+			{
+			cout << "\t";
+			cout << ".";
+			}
+		    cout << endl;
+		    }
+		}
+
+
+	    }
+
+
+	void run_grep(std::istream& in,const set<string>& all_descendants)
+	    {
+	    Tokenizer tab('\t');
+	    vector<string> tokens;
+	    string line;
+
+	    for(;;)
+		{
+		if(!getline(in,line,'\n')) break;
+		if(line.empty()) continue;
+		if(line[0]=='#')
+		    {
+		    cout << line << endl;
+		    continue;
+		    }
+		tab.split(line,tokens);
+		if((int32_t)tokens.size()<=go_acn_column)
+		    {
+		    cerr << "column "<< (go_acn_column+1)<< " missing in "<< line << endl;
+		    continue;
+		    }
+		bool found=false;
+		if( (all_descendants.find(tokens[go_acn_column])!=all_descendants.end())!=inverse_result)
+		    {
+		    cout << line << endl;
+		    }
+		}
+
+
+	    }
 
 	virtual void usage(ostream& out,int argc,char** argv)
 	    {
@@ -339,14 +516,43 @@ class GoDatabaseManager:public AbstractApplication
 	    out << " "<< argv[0] << " loadgoa -f db.sqlite (stdin|rdf-files)\n";
 	    out << " Options for :\n";
 	    out << "   -f (file) sqlite filename. (REQUIRED).\n";
+
 	    out << "\nProgram: desc\n\n";
-	    out << " print the descendants of a given node.\n";
+	    out << " print the descendants (children) of a given node.\n";
 	    out << "  Usage\n";
 	    out << " "<< argv[0] << " desc -f db.sqlite term1 term2 ... termn\n";
 	    out << " Options for :\n";
 	    out << "   -f (file) sqlite filename. (REQUIRED).\n";
 	    out << "   -r (rel) add a go relationship (OPTIONAL, default: it adds \"is_a\").\n";
-	    out << "   -t output: xml, goa\n";
+	    out << "   -t output: xml, goa ,tsv ,acn\n";
+
+	    out << "\nProgram: asc\n\n";
+	    out << " print the ascendants (parents) of a given node.\n";
+	    out << "  Usage\n";
+	    out << " "<< argv[0] << " desc -f db.sqlite term1 term2 ... termn\n";
+	    out << " Options for :\n";
+	    out << "   -f (file) sqlite filename. (REQUIRED).\n";
+	    out << "   -r (rel) add a go relationship (OPTIONAL, default: it adds \"is_a\").\n";
+	    out << "   -t output: xml, goa, tsv ,acn\n";
+
+
+	    out << "\nProgram: goa\n\n";
+	    out << " appends the GOA annotation\n";
+	    out << "  Usage\n";
+	    out << " "<< argv[0] << " goa [options] (file1 file2 .. | stdin)\n";
+	    out << " Options for :\n";
+	    out << "   -f (file) sqlite filename. (REQUIRED).\n";
+	    out << "   -c (column index) REQUIRED. The observed column\n";
+	    out << "\nProgram: grep\n\n";
+	    out << " filters line having a GO acn descendant of one or more go term.\n";
+	    out << "  Usage\n";
+	    out << " "<< argv[0] << " grep [options] (file1 file2 .. | stdin)\n";
+	    out << " Options for grep:\n";
+	    out << "   -f (file) sqlite filename. (REQUIRED).\n";
+	    out << "   -c (column index) REQUIRED. The column containing a GO:acn\n";
+	    out << "   -v inverse selection\n";
+	    out << "   -t (GO:acn) add a GO term in the filter (One is REQUIRED).\n";
+	    out << "   -r (rel) add a go relationship (OPTIONAL, default: it adds \"is_a\").\n";
 	    out << endl;
 	    }
 
@@ -466,7 +672,7 @@ class GoDatabaseManager:public AbstractApplication
 
 
 
-	int main_descendants(int argc,char** argv,int optind)
+	int main_descendants_or_ascendant(int argc,char** argv,int optind,bool is_descendant)
 	    {
 	    int output_format=0;
 	    set<string> relationships;
@@ -483,7 +689,14 @@ class GoDatabaseManager:public AbstractApplication
 		    }
 		else if(strcmp(argv[optind],"-r")==0 && optind+1<argc)
 		    {
-		    relationships.insert(argv[++optind]);
+		    const char* s=(argv[++optind]);
+		    if(!is_relation_ontology(s))
+			{
+			cerr << "Not in relation ontology: "<< s << endl;
+			this->usage(cerr,argc,argv);
+			return (EXIT_FAILURE);
+			}
+		    relationships.insert(s);
 		    }
 		else if(strcmp(argv[optind],"-t")==0 && optind+1<argc)
 		    {
@@ -496,6 +709,14 @@ class GoDatabaseManager:public AbstractApplication
 		    	{
 		    	output_format=2;
 		    	}
+		    else if(strcmp(argv[optind],"tsv")==0)
+			{
+			output_format=3;
+			}
+		    else
+			{
+			output_format=0;
+			}
 		    }
 		else if(argv[optind][0]=='-')
 		    {
@@ -541,7 +762,15 @@ class GoDatabaseManager:public AbstractApplication
 	    set<string> result;
 	    for(set<string>::iterator r=terms.begin();r!=terms.end();++r)
 		{
-		auto_ptr<set<string> > ret= this->find_descendants(r->c_str(),relationships);
+		auto_ptr<set<string> > ret(0);
+		if(is_descendant)
+		    {
+		    ret=this->find_descendants(r->c_str(),relationships);
+		    }
+		else
+		    {
+		    ret=this->find_ascendants(r->c_str(),relationships);
+		    }
 		result.insert(ret->begin(),ret->end());
 		}
 	    /* XML format */
@@ -590,11 +819,234 @@ class GoDatabaseManager:public AbstractApplication
 		    	}
 		    }
 	    	}
+	    else if(output_format==3)
+		{
+		cout << "#go:accession\tgo.name\tgo.def"<< endl;
+		for(set<string>::iterator r=result.begin();r!=result.end();++r)
+		    {
+		    xmlDocPtr dom=getTermDomByAcn(r->c_str());
+		    if(dom==0)
+			{
+			cout << (*r)<< "\t.\t."<< endl;
+			continue;
+			}
+		    cout << (*r);
+		    for(int k=0;k<2;++k)
+			    {
+			    cout << "\t";
+			    xmlNodePtr term=xmlDocGetRootElement(dom);
+			    xmlNodePtr elmt=first(term,BAD_CAST (k==0?"name":"definition"));
+			    if(elmt==0)
+				{
+				cout << ".";
+				continue;
+				}
+			    xmlChar *content=xmlNodeGetContent(elmt);
+			    if(content==0)
+				{
+				cout << ".";
+				}
+			    else
+				{
+				cout << CEscape((const char*) content);
+				xmlFree(content);
+				}
+			}
+		    cout << endl;
+		    xmlFreeDoc(dom);
+		    }
+		}
 	    else
 		{
 		for(set<string>::iterator r=result.begin();r!=result.end();++r)
 		    {
 		    cout << (*r) << endl;
+		    }
+		}
+	    close();
+	    return EXIT_SUCCESS;
+	    }
+
+
+
+
+	int main_goa(int argc,char** argv,int optind)
+	    {
+	    while(optind < argc)
+		{
+		if(std::strcmp(argv[optind],"-h")==0)
+		    {
+		    this->usage(cerr,argc,argv);
+		    return (EXIT_FAILURE);
+		    }
+		else if(strcmp(argv[optind],"-f")==0 && optind+1<argc)
+		    {
+		    dataasename=argv[++optind];
+		    }
+		else if(strcmp(argv[optind],"-c")==0 && optind+1<argc)
+		    {
+		    if(!numeric_cast<int32_t>(argv[++optind],&goa_keyword_column) || goa_keyword_column<1)
+			{
+			cerr << "Bad column index "<< argv[optind]<< endl;
+			this->usage(cerr,argc,argv);
+			return (EXIT_FAILURE);
+			}
+		    goa_keyword_column--;
+		    }
+		else if(argv[optind][0]=='-')
+		    {
+		    cerr << "unknown option '"<< argv[optind]<<"'\n";
+		    this->usage(cerr,argc,argv);
+		    return (EXIT_FAILURE);
+		    }
+		else
+		    {
+		    break;
+		    }
+		++optind;
+		}
+	    if(dataasename==NULL)
+		{
+		cerr << "Undefined sqlite database."<< endl;
+		this->usage(cerr,argc,argv);
+		return (EXIT_FAILURE);
+		}
+	    if(goa_keyword_column<0)
+		{
+		cerr << "Undefined column."<< endl;
+		this->usage(cerr,argc,argv);
+		return (EXIT_FAILURE);
+		}
+	    open(true);
+	    if(optind==argc)
+		{
+		igzstreambuf buf;
+		istream in(&buf);
+		this->run_goa(in);
+		}
+	    else
+		{
+		while(optind< argc)
+		    {
+		    if(AbstractApplication::stopping()) break;
+		    char* input=argv[optind++];
+		    igzstreambuf buf(input);
+		    istream in(&buf);
+		    this->run_goa(in);
+		    buf.close();
+		    }
+		}
+	    close();
+	    return EXIT_SUCCESS;
+	    }
+
+
+	int main_grep(int argc,char** argv,int optind)
+	    {
+	    set<string> relationships;
+	    set<string> terms;
+	    while(optind < argc)
+		{
+		if(std::strcmp(argv[optind],"-h")==0)
+		    {
+		    this->usage(cerr,argc,argv);
+		    return (EXIT_FAILURE);
+		    }
+		else if(strcmp(argv[optind],"-f")==0 && optind+1<argc)
+		    {
+		    dataasename=argv[++optind];
+		    }
+		else if(strcmp(argv[optind],"-t")==0 && optind+1<argc)
+		    {
+		    terms.insert(argv[++optind]);
+		    }
+		else if(strcmp(argv[optind],"-v")==0)
+		    {
+		    this->inverse_result=true;
+		    }
+		else if(strcmp(argv[optind],"-r")==0 && optind+1<argc)
+		    {
+		    const char* s=(argv[++optind]);
+		    if(!is_relation_ontology(s))
+			{
+			cerr << "Not in relation ontology: "<< s << endl;
+			this->usage(cerr,argc,argv);
+			return (EXIT_FAILURE);
+			}
+		    relationships.insert(s);
+		    }
+		else if(strcmp(argv[optind],"-c")==0 && optind+1<argc)
+		    {
+		    if(!numeric_cast<int32_t>(argv[++optind],&go_acn_column) || go_acn_column<1)
+			{
+			cerr << "Bad column index "<< argv[optind]<< endl;
+			this->usage(cerr,argc,argv);
+			return (EXIT_FAILURE);
+			}
+		    go_acn_column--;
+		    }
+		else if(argv[optind][0]=='-')
+		    {
+		    cerr << "unknown option '"<< argv[optind]<<"'\n";
+		    this->usage(cerr,argc,argv);
+		    return (EXIT_FAILURE);
+		    }
+		else
+		    {
+		    break;
+		    }
+		++optind;
+		}
+	    if(dataasename==NULL)
+		{
+		cerr << "Undefined sqlite database."<< endl;
+		this->usage(cerr,argc,argv);
+		return (EXIT_FAILURE);
+		}
+	    if(go_acn_column<0)
+		{
+		cerr << "Undefined column."<< endl;
+		this->usage(cerr,argc,argv);
+		return (EXIT_FAILURE);
+		}
+	    if(terms.empty())
+		{
+		cerr << "Undefined go-terms."<< endl;
+		this->usage(cerr,argc,argv);
+		return (EXIT_FAILURE);
+		}
+	    if(relationships.empty())
+		{
+		relationships.insert("is_a");
+		}
+
+
+
+	    open(true);
+
+	    set<string> result;
+	    for(set<string>::iterator r=terms.begin();r!=terms.end();++r)
+		{
+		auto_ptr<set<string> > ret= this->find_descendants(r->c_str(),relationships);
+		result.insert(ret->begin(),ret->end());
+		}
+
+	    if(optind==argc)
+		{
+		igzstreambuf buf;
+		istream in(&buf);
+		this->run_grep(in,result);
+		}
+	    else
+		{
+		while(optind< argc)
+		    {
+		    if(AbstractApplication::stopping()) break;
+		    char* input=argv[optind++];
+		    igzstreambuf buf(input);
+		    istream in(&buf);
+		    this->run_grep(in,result);
+		    buf.close();
 		    }
 		}
 	    close();
@@ -641,7 +1093,19 @@ class GoDatabaseManager:public AbstractApplication
 		}
 	    else if(strcmp(argv[optind],"desc")==0)
 		{
-		return main_descendants(argc,argv,optind+1);
+		return main_descendants_or_ascendant(argc,argv,optind+1,true);
+		}
+	    else if(strcmp(argv[optind],"goa")==0)
+		{
+		return main_goa(argc,argv,optind+1);
+		}
+	    else if(strcmp(argv[optind],"grep")==0)
+		{
+		return main_grep(argc,argv,optind+1);
+		}
+	    else if(strcmp(argv[optind],"asc")==0)
+		{
+		return main_descendants_or_ascendant(argc,argv,optind+1,false);
 		}
 	    else
 		{
