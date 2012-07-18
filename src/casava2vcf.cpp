99895f7eab21fae552a4343613f38b18037e0350
plindenbaum.u915 <plindenbaum.u915@b48bb709-da2a-0096-c5f6-d1abd257d063>
2012-07-18 07:42:27 +0000

    changes by andre blavier

--- src/casava2vcf.cpp
+++ src/casava2vcf.cpp
@@ -1,6 +1,7 @@
 /**
  * Author:
  *	Pierre Lindenbaum PhD
+ *	This program was later edited by Andre Blavier ( interactive-biosoftware.com ) but I didn't test it.
  * Contact:
  *	plindenbaum@yahoo.fr
  * WWW:
@@ -17,6 +18,7 @@
 #include <limits>
 #include <string>
 #include <stdint.h>
+#include <boost/tokenizer.hpp>
 #include "throw.h"
 #include "tokenizer.h"
 #include "numeric_cast.h"
@@ -44,6 +46,32 @@ class CasavaToVCF
 		return getline(in,line,'\n');
 		}
 	    }
+
+	string parseChrom(const string& src)
+	{
+		// Try to extract the chrom name out of e.g. "chr17.fa".
+		// Return empty string if not found.
+		// (A poor man's implementation, without regex)
+		if (src.empty()) return string();
+		string prefix = src.substr(0, 3);
+		string src2 = src;
+		if (prefix == "chr" || prefix == "Chr" || prefix == "CHR")
+			src2.erase(0, 3);
+
+		if (src2.empty()) return string();
+		if (src2.at(0) == 'X' || src2.at(0) == 'x') return "X";
+		if (src2.at(0) == 'x' || src2.at(0) == 'y') return "Y";
+		if (src2.at(0) == 'M' || src2.at(0) == 'm') return "MT";  // a bit risky?
+
+		string chrom;
+		for (size_t i = 0; i < src2.size(); ++i)
+		{
+			char c = src2.at(i);
+			if (isdigit(c)) chrom += c;
+			else break;
+		}
+		return chrom;
+	}
 
     public:
 	CasavaToVCF()
@@ -87,7 +115,7 @@ class CasavaToVCF
 
 		if(genotypes.empty()) continue;
 
-		cout << tokens[0] << "\t" ; //CHROM
+		cout << parseChrom(tokens[0]) << "\t" ; //CHROM
 		cout << tokens[1] << "\t" ; //POS
 		cout << "." << "\t" ; //ID
 		cout << tokens[4] << "\t" ; //REF
@@ -101,12 +129,12 @@ class CasavaToVCF
 		cout << tokens[5]  << "\t" ; //QUAL
 		cout << ".\t" ; //FILTER
 		cout << "DP="<< tokens[2];
-		cout << ";bcalls_used="<< tokens[2];
+//		cout << ";bcalls_used="<< tokens[2];
 		cout << ";Q_snp="<< tokens[5];
 		cout << ";max_gt="<< tokens[6];
 		cout << ";Q_max_gt="<< tokens[7];
 		cout << ";max_gt_poly_site="<< tokens[8];
-		cout << ";Q_max_gt_poly_site="<< tokens[8];
+		cout << ";Q_max_gt_poly_site="<< tokens[9];
 		cout << ";A_used="<< tokens[10];
 		cout << ";C_used="<< tokens[11];
 		cout << ";G_used="<< tokens[12];
@@ -167,7 +195,7 @@ class CasavaToVCF
 		if(left.size()!=right.size()) THROW("Bad indel "<<line);
 
 
-		cout << tokens[0] << "\t" ; //CHROM
+		cout << parseChrom(tokens[0]) << "\t" ; //CHROM
 		int position1;
 		if(!numeric_cast<int>(tokens[1].c_str(),&position1) || position1<1)
 		    {
@@ -238,9 +266,8 @@ class CasavaToVCF
 		}
 	    }
 
-#define VERIFYCOL(k,i) \
-	if((int)tokens.size()<=i) THROW("Expected at least "<< (i+1) << "columns in "<< line << "  but got "<< tokens.size());\
-	if(tokens[i].compare(k)!=0) THROW("Expected column "<< i << " to be "<< k << " but got "<< tokens[i]<< " in "<< line)
+#define CHECK_NEXT_COL(k) \
+	++tok_it; if (tok_it == tokens.end() || *tok_it != k) THROW(col_err_msg);
 
 	void run(std::istream& in)
 	    {
@@ -249,57 +276,73 @@ class CasavaToVCF
 	    while(next(in,line))
 		{
 		if(line.empty()) continue;
-		tokenizer.split(line,tokens);
-		if(tokens.size()>2 && tokens[2].compare("bcalls_used")==0)
-		    {
-		    VERIFYCOL("seq_name",0);
-		    VERIFYCOL("pos",1);
-		    VERIFYCOL("bcalls_used",2);
-		    VERIFYCOL("bcalls_filt",3);
-		    VERIFYCOL("ref",4);
-		    VERIFYCOL("Q(snp)",5);
-		    VERIFYCOL("max_gt",6);
-		    VERIFYCOL("Q(max_gt)",7);
-		    VERIFYCOL("max_gt|poly_site",8);
-		    VERIFYCOL("Q(max_gt|poly_site)",9);
-		    VERIFYCOL("A_used",10);
-		    VERIFYCOL("C_used",11);
-		    VERIFYCOL("G_used",12);
-		    VERIFYCOL("T_used",13);
-		    buffer.release();
-		    run_snp(in);
-		    }
-		else if(tokens.size()>2 && tokens[2].compare("type")==0)
-		    {
-		    VERIFYCOL("seq_name",0);
-		    VERIFYCOL("pos",1);
-		    VERIFYCOL("type",2);
-		    VERIFYCOL("ref_upstream",3);
-		    VERIFYCOL("ref/indel",4);
-		    VERIFYCOL("ref_downstream",5);
-		    VERIFYCOL("Q(indel)",6);
-		    VERIFYCOL("max_gtype",7);
-		    VERIFYCOL("Q(max_gtype)",8);
-		    VERIFYCOL("depth",9);
-		    VERIFYCOL("alt_reads",10);
-		    VERIFYCOL("indel_reads",11);
-		    VERIFYCOL("other_reads",12);
-		    VERIFYCOL("repeat_unit",13);
-		    VERIFYCOL("ref_repeat_count",14);
-		    VERIFYCOL("indel_repeat_count",15);
-		    buffer.release();
-		    run_indel(in);
-		    }
-		else
-		    {
-		    THROW("bad header in \""<< line << "\"");
-		    }
-
+		if (line.substr(0, 2) == "#$")		// header line
+		{
+			typedef boost::tokenizer<boost::char_separator<char> > boost_tokenizer;
+			boost::char_separator<char> sep(" ");
+			boost_tokenizer tokens(line, sep);
+			boost_tokenizer::iterator tok_it = tokens.begin();
+			++tok_it;  // skip "#$"
+			if (tok_it == tokens.end()) continue;
+			else if (*tok_it == "COLUMNS")
+			{
+				cout << "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t__SAMPLE__" << endl;
+				static const char* col_err_msg = "Invalid COLUMNS header line";
+				CHECK_NEXT_COL("seq_name");
+				CHECK_NEXT_COL("pos");
+				++tok_it; if (tok_it == tokens.end()) THROW(col_err_msg);
+				if (*tok_it == "bcalls_used")
+				{
+					CHECK_NEXT_COL("bcalls_filt");
+					CHECK_NEXT_COL("ref");
+					CHECK_NEXT_COL("Q(snp)");
+					CHECK_NEXT_COL("max_gt");
+					CHECK_NEXT_COL("Q(max_gt)");
+					CHECK_NEXT_COL("max_gt|poly_site");
+					CHECK_NEXT_COL("Q(max_gt|poly_site)");
+					CHECK_NEXT_COL("A_used");
+					CHECK_NEXT_COL("C_used");
+					CHECK_NEXT_COL("G_used");
+					CHECK_NEXT_COL("T_used");
+					buffer.release();
+					run_snp(in);
+				}
+				else if (*tok_it == "type")
+				{
+					CHECK_NEXT_COL("ref_upstream");
+					CHECK_NEXT_COL("ref/indel");
+					CHECK_NEXT_COL("ref_downstream");
+					CHECK_NEXT_COL("Q(indel)");
+					CHECK_NEXT_COL("max_gtype");
+					CHECK_NEXT_COL("Q(max_gtype)");
+					CHECK_NEXT_COL("depth");
+					CHECK_NEXT_COL("alt_reads");
+					CHECK_NEXT_COL("indel_reads");
+					CHECK_NEXT_COL("other_reads");
+					CHECK_NEXT_COL("repeat_unit");
+					CHECK_NEXT_COL("ref_repeat_count");
+					CHECK_NEXT_COL("indel_repeat_count");
+					buffer.release();
+					run_indel(in);
+				}
+				else THROW(col_err_msg);
+			}
+			else	// key-value pairs
+			{
+				cout << "##" << *tok_it << "=";
+				++tok_it;
+				for ( ; tok_it != tokens.end(); ++tok_it)
+					cout << *tok_it << " ";
+				cout << endl;
+			}
+		}
+		else if (line.substr(0, 1) == "#") continue;
+		else THROW("Invalid line \""<< line << "\"");
 		}
 	    }
 	void usage(ostream& out,int argc,char **argv)
 		{
-		out << argv[0] << " Pierre Lindenbaum PHD. 2012.\n";
+		out << argv[0] << " Pierre Lindenbaum PHD, Andre Blavier 2012.\n";
 		out << "Compilation: "<<__DATE__<<"  at "<< __TIME__<<".\n";
 		out << "Usage:\n\t"<< argv[0] << " [options] files.txt\n";
 		}
@@ -334,18 +377,28 @@ class CasavaToVCF
 		    ++optind;
 		    }
 	    cout << "##fileformat=VCFv4.1"<< endl;
-	    cout << "##" << argv[0] << endl;
+	    cout << "##VCFConverter=casava2vcf" << endl;
 	    cout << "##INFO=<ID=DP,Number=1,Type=Integer,Description=\"Basecalls used to make the genotype call for this site\">" << endl;
-	    cout << "##INFO=<ID=bcalls_filt,Number=1,Type=Integer,Description=\" Basecalls mapped to the site but filtered out before genotype calling\">" << endl;
+//	    cout << "##INFO=<ID=bcalls_used,Number=1,Type=Integer,Description=\"Basecalls used to make the genotype call for this site\">" << endl;
+	    cout << "##INFO=<ID=bcalls_filt,Number=1,Type=Integer,Description=\"Basecalls mapped to the site but filtered out before genotype calling\">" << endl;
 	    cout << "##INFO=<ID=Q_snp,Number=1,Type=String,Description=\"A Q-value expressing the probability of the homozygous reference genotype\">" << endl;
-	    cout << "##INFO=<ID=max_gt,Number=1,Type=Integer,Description=\"The most likely genotype\">" << endl;
+	    cout << "##INFO=<ID=max_gt,Number=1,Type=String,Description=\"The most likely genotype\">" << endl;
 	    cout << "##INFO=<ID=Q_max_gt,Number=1,Type=Integer,Description=\"Q-value expressing the probability that the genotype is not the most likely genotype\">" << endl;
-	    cout << "##INFO=<ID=max_gt_poly_site,Number=1,Type=Integer,Description=\"The most likely genotype assuming this site is polymorphic with an expected allele frequency of 0.5\">" << endl;
-	    cout << "##INFO=<ID=Q_max_gt_poly_site,Number=1,Type=Integer,Description=\"Q(max_gt|poly_site)\">" << endl;
-	    cout << "##INFO=<ID=A_used,Number=1,Type=Integer,Description=\"A_used\">" << endl;
-	    cout << "##INFO=<ID=C_used,Number=1,Type=Integer,Description=\"C_used\">" << endl;
-	    cout << "##INFO=<ID=G_used,Number=1,Type=Integer,Description=\"G_used\">" << endl;
-	    cout << "##INFO=<ID=T_used,Number=1,Type=Integer,Description=\"T_used\">" << endl;
+	    cout << "##INFO=<ID=max_gt_poly_site,Number=1,Type=String,Description=\"The most likely genotype assuming this site is polymorphic with an expected allele frequency of 0.5\">" << endl;
+	    cout << "##INFO=<ID=Q_max_gt_poly_site,Number=1,Type=Integer,Description=\"A Q-value expressing the probability that the genotype is not the most likely genotype above assuming this site is polymorphic\">" << endl;
+	    cout << "##INFO=<ID=A_used,Number=1,Type=Integer,Description=\"'A' basecalls used\">" << endl;
+	    cout << "##INFO=<ID=C_used,Number=1,Type=Integer,Description=\"'C' basecalls used\">" << endl;
+	    cout << "##INFO=<ID=G_used,Number=1,Type=Integer,Description=\"'G' basecalls used\">" << endl;
+	    cout << "##INFO=<ID=T_used,Number=1,Type=Integer,Description=\"'T' basecalls used\">" << endl;
+
+	    cout << "##INFO=<ID=INSERTION,Number=1,Type=Integer,Description=\"Number of inserted bases\">" << endl;
+	    cout << "##INFO=<ID=DELETION,Number=1,Type=Integer,Description=\"Number of deleted bases\">" << endl;
+	    cout << "##INFO=<ID=alt_reads,Number=1,Type=Integer,Description=\"Number of reads strongly supporting either the reference path or an alternate indelpath\">" << endl;
+	    cout << "##INFO=<ID=indel_reads,Number=1,Type=Integer,Description=\"Number of reads strongly supporting the indel path\">" << endl;
+	    cout << "##INFO=<ID=other_reads,Number=1,Type=Integer,Description=\"Number of reads intersecting the indel, but not strongly supporting either the reference or any one indelpath\">" << endl;
+	    cout << "##INFO=<ID=repeat_unit,Number=1,Type=String,Description=\"The smallest repeating sequence unit within the inserted or deleted sequence. For breakpoints this field is set to the value 'N/A'\">" << endl;
+	    cout << "##INFO=<ID=ref_repeat_count,Number=1,Type=Integer,Description=\"Number of times the repeat_unit sequence is contiguously repeated starting from the indel start position in the reference case\">" << endl;
+	    cout << "##INFO=<ID=indel_repeat_count,Number=1,Type=Integer,Description=\"Number of times the repeat_unit sequence is contiguously repeated starting from the indel start position in the indel case\">" << endl;
 
 cout << "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">" << endl;
 cout << "##FORMAT=<ID=GQ,Number=1,Type=Integer,Description=\"Genotype Quality\">" << endl;
@@ -357,7 +410,6 @@ cout << "##FORMAT=<ID=DP,Number=1,Type=Integer,Description=\"# high-quality base
 cout << "##FORMAT=<ID=SP,Number=1,Type=Integer,Description=\"Phred-scaled strand bias P-value\">" << endl;
 cout << "##FORMAT=<ID=PL,Number=-1,Type=Integer,Description=\"List of Phred-scaled genotype likelihoods, number of values is (#ALT+1)*(#ALT+2)/2">\" << endl;
 */
-	    cout << "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t__SAMPLE__" << endl;
 
 	    if(optind==argc)
 		    {
